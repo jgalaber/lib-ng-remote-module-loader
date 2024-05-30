59b388c9e64e4377ad675cff745a4f2711c75131
jgalaber <jgalaberdev@gmail.com>
2024-05-30 22:54:40 +0200

    feat: 🎸 add remote-module-loader library scaffolding

---
+++ projects/remote-module-loader/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of remote-module-loader
+ */
+
+export * from './lib/remote-module-loader.service';
+export * from './lib/remote-module-loader.component';
+export * from './lib/remote-module-loader.module';
