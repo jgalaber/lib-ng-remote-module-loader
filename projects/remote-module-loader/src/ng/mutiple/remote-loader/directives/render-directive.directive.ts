c80c8e214d4371845e450c5e2abe15fbeb5b0297
jgalaber <jgalaberdev@gmail.com>
2024-06-08 17:59:16 +0200

    refactor: 💡 make more simple the remote module load

--- projects/remote-module-loader/src/ng/mutiple/remote-loader/directives/render-directive.directive.ts
+++ projects/remote-module-loader/src/ng/mutiple/remote-loader/directives/render-directive.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, Input, NgModuleRef, OnDestroy, ViewContainerRef } from '@angular/core';
 import { LoadableApp } from '../../remote-loader.types';
 import { Router } from '@angular/router';
+import { RouterInitializerComponent } from '../components/router-initializer/router-initializer.component';
 
 @Directive({
   selector: '[renderApp]',
@@ -17,20 +18,8 @@ export class RenderAppDirective implements OnDestroy {
     const injector = ngModuleRef.injector;
     const router = ngModuleRef.injector.get(Router, null);
 
-    if (!ngModuleRef.instance.EntryComponent) {
-      if (router) {
-        router.initialNavigation();
-        return;
-      } else {
-        throw Error(
-          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-          `Cannot load app: ${ngModuleRef} because the EntryComponent is not defined in the Module and no router is available.`,
-        );
-      }
-    }
-
     const componentRef = this.viewContainerRef.createComponent(
-      ngModuleRef.instance.EntryComponent,
+      ngModuleRef.instance.EntryComponent ?? RouterInitializerComponent,
       { injector },
     );
     componentRef.changeDetectorRef.detectChanges();
